f6b9985038c21981f555dbd1cdbd8e25fc8d53e6
Aron Lee <yujun9021@mjc.ac.kr>
2024-04-10 12:59:39 +0900

    version1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/Ap.js</code> and save to reload.
+          Edit <code>src/Apafdfasfasdfadfasdfasd.js</code> and save to reload.
         </p>
         <a
           className="App-link"
