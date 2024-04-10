500ce0b260107e36df6deeefd294619bebc5cf42
Aron Lee <yujun9021@mjc.ac.kr>
2024-04-10 13:27:37 +0900

    fix 2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React f'dfd'adslkf;lsks;lfkdojgdjfd's
-          fuck your selfs
+          fuck your selfsvsdvsvds
         </a>
       </header>
     </div>
