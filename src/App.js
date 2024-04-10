1c3361289f02d1b64b833f3e5f3f66ba6d167c03
hperson12 <jikjilk345@gmail.com>
2024-04-10 13:13:33 +0900

    version23

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React f'dfd'adslkf;lsks;lfkdfdfsfa
+          Learn React f'dfd'adslkf;lsks;lfkdfdf
         </a>
       </header>
     </div>
